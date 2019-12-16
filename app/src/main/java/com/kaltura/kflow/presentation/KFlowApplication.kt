dc7b44231e2e394295bbdeba1525f1a02012ff58
Alexander Litvinenko <a.litvinenko@synergetica.co>
2019-12-16 15:39:52 +0200

    refactor KFlowApplication

---
+++ app/src/main/java/com/kaltura/kflow/presentation/KFlowApplication.kt
@@ -0,0 +1,23 @@
+package com.kaltura.kflow.presentation
+
+import android.app.Application
+import com.kaltura.client.Configuration
+import com.kaltura.kflow.manager.PhoenixApiManager
+import com.kaltura.kflow.manager.PreferenceManager
+
+/**
+ * Created by alex_lytvynenko on 2019-06-25.
+ */
+class KFlowApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initApiClient()
+    }
+
+    private fun initApiClient() {
+        val config = Configuration()
+        config.endpoint = PreferenceManager.getInstance(this).baseUrl
+        PhoenixApiManager.getClient().connectionConfiguration = config
+        PhoenixApiManager.getClient().ks = PreferenceManager.getInstance(this).ks
+    }
+}
