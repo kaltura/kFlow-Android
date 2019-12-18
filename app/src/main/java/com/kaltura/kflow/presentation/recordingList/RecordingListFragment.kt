ce5a9b764eedeab5dbd7d05c9a6d944bfae945dd
Alex Litvinenko <a.litvinenko@synergetica.co>
2019-12-18 15:45:27 +0200

    refactor RecordingsList fragment and adapter

---
+++ app/src/main/java/com/kaltura/kflow/presentation/recordingList/RecordingListFragment.kt
@@ -0,0 +1,45 @@
+package com.kaltura.kflow.presentation.recordingList
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kaltura.client.enums.RecordingStatus
+import com.kaltura.client.types.Recording
+import com.kaltura.kflow.R
+import com.kaltura.kflow.presentation.extension.instanceOf
+import com.kaltura.kflow.presentation.extension.replaceFragment
+import com.kaltura.kflow.presentation.main.MainActivity
+import com.kaltura.kflow.presentation.player.PlayerFragment
+import kotlinx.android.synthetic.main.fragment_recording_list.*
+import java.util.*
+
+/**
+ * Created by alex_lytvynenko on 30.11.2018.
+ */
+class RecordingListFragment : Fragment(R.layout.fragment_recording_list) {
+
+    companion object {
+        const val ARG_RECORDINGS = "extra_recordings"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as MainActivity).supportActionBar?.title = "Recording list"
+        initList()
+    }
+
+    private fun initList() {
+        list.setHasFixedSize(true)
+        list.layoutManager = LinearLayoutManager(requireContext())
+        list.addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
+        val recordings = arguments?.getSerializable(ARG_RECORDINGS) as? ArrayList<Recording> ?: arrayListOf()
+        list.adapter = RecordingListAdapter(recordings).apply {
+            recordingClickListener = {
+                if (it.status == RecordingStatus.RECORDED)
+                    replaceFragment(instanceOf<PlayerFragment>(PlayerFragment.ARG_RECORDING to it), addToBackStack = true)
+            }
+        }
+    }
+}
