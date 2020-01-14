4be7b11652d16ef06da0ad606de1c6a080d87f55
Alex Litvinenko <a.litvinenko@synergetica.co>
2020-01-14 16:50:52 +0200

    add SubscriptionViewModel

--- app/src/main/java/com/kaltura/kflow/presentation/subscription/SubscriptionFragment.kt
+++ app/src/main/java/com/kaltura/kflow/presentation/subscription/SubscriptionFragment.kt
@@ -2,19 +2,12 @@ package com.kaltura.kflow.presentation.subscription
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.kaltura.client.enums.AssetOrderBy
-import com.kaltura.client.enums.EntityReferenceBy
-import com.kaltura.client.enums.TransactionType
-import com.kaltura.client.services.AssetService
-import com.kaltura.client.services.EntitlementService
-import com.kaltura.client.services.SubscriptionService
 import com.kaltura.client.types.*
-import com.kaltura.client.utils.request.MultiRequestBuilder
 import com.kaltura.kflow.R
 import com.kaltura.kflow.entity.ParentRecyclerViewItem
-import com.kaltura.kflow.manager.PhoenixApiManager
 import com.kaltura.kflow.presentation.assetList.AssetListFragment
 import com.kaltura.kflow.presentation.debug.DebugFragment
 import com.kaltura.kflow.presentation.debug.DebugView
@@ -29,12 +22,14 @@ import kotlin.collections.ArrayList
  */
 class SubscriptionFragment : DebugFragment(R.layout.fragment_subscription) {
 
+    private val viewModel: SubscriptionViewModel by viewModels()
     private val assets = arrayListOf<Asset>()
     private var subscriptionListAdapter = SubscriptionListAdapter(arrayListOf()).apply {
         packageGetSubscriptionListener = ::onPackageGetSubscriptionClicked
         subscriptionListener = ::onSubscriptionClicked
     }
     private lateinit var progressDialog: ProgressDialog
+    private var selectedPackageBaseId: Double = 0.0
 
     override fun debugView(): DebugView = debugView
 
@@ -59,7 +54,21 @@ class SubscriptionFragment : DebugFragment(R.layout.fragment_subscription) {
         progressDialog = ProgressDialog(requireContext())
     }
 
-    override fun subscribeUI() {}
+    override fun subscribeUI() {
+        observeResource(viewModel.assetList) {
+            assets.addAll(it)
+            showAssets.text = getQuantityString(R.plurals.show_assets, assets.size)
+            showAssets.visible()
+        }
+        observeResource(viewModel.subscriptionList) {
+            subscriptionListAdapter.addSubscriptionToPackage(selectedPackageBaseId, it)
+        }
+        observeResource(viewModel.assetsInSubscription) {
+            hideLoadingDialog()
+            if (it.isEmpty()) toast("No assets in this subscription")
+            else navigate(SubscriptionFragmentDirections.navigateToAssetList(), AssetListFragment.ARG_ASSETS to it)
+        }
+    }
 
     private fun initList() {
         packageList.isNestedScrollingEnabled = false
@@ -73,87 +82,29 @@ class SubscriptionFragment : DebugFragment(R.layout.fragment_subscription) {
             assets.clear()
             showAssets.gone()
             packageList.gone()
-            val filter = SearchAssetFilter().apply {
-                orderBy = AssetOrderBy.START_DATE_DESC.value
-                kSql = "Base ID > \'0\'"
-                typeIn = packageType
-            }
-
-            val filterPager = FilterPager().apply {
-                pageIndex = 1
-                pageSize = 40
-            }
-
             clearDebugView()
-            PhoenixApiManager.execute(AssetService.list(filter, filterPager).setCompletion {
-                if (it.isSuccess) {
-                    if (it.results.objects != null) assets.addAll(it.results.objects)
-                    showAssets.text = getQuantityString(R.plurals.show_assets, assets.size)
-                    showAssets.visible()
-                }
-            })
+            viewModel.getPackageList(packageType)
         }
     }
 
     private fun makeGetEntitlementListRequest() {
         withInternetConnection {
-            val filter = EntitlementFilter().apply {
-                entityReferenceEqual = EntityReferenceBy.HOUSEHOLD
-                productTypeEqual = TransactionType.SUBSCRIPTION
-                isExpiredEqual = true
-            }
-
-            val filterPager = FilterPager().apply {
-                pageIndex = 1
-                pageSize = 40
-            }
-
             clearDebugView()
-            PhoenixApiManager.execute(EntitlementService.list(filter, filterPager))
+            viewModel.getEntitlementList()
         }
     }
 
     private fun getSubscriptionRequest(subscriptionBaseId: String) {
         withInternetConnection {
-            val subscriptionFilter = SubscriptionFilter().apply { subscriptionIdIn = subscriptionBaseId }
-            val filterPager = FilterPager().apply {
-                pageIndex = 1
-                pageSize = 40
-            }
-
             clearDebugView()
-            PhoenixApiManager.execute(SubscriptionService.list(subscriptionFilter, filterPager).setCompletion {
-                if (it.isSuccess) {
-                    subscriptionListAdapter.addSubscriptionToPackage(subscriptionBaseId.toDouble(), it.results.objects as ArrayList<Subscription>)
-                }
-            })
+            viewModel.getSubscription(subscriptionBaseId)
         }
     }
 
     private fun getAssetsInSubscription(subscriptionChannelsId: ArrayList<Long>) {
         withInternetConnection {
-            val multiRequestBuilder = MultiRequestBuilder()
-            val channelFilter = ChannelFilter()
-            val filterPager = FilterPager().apply {
-                pageIndex = 1
-                pageSize = 40
-            }
-
-            subscriptionChannelsId.forEach {
-                channelFilter.idEqual = it.toInt()
-                multiRequestBuilder.add(AssetService.list(channelFilter, filterPager))
-            }
-            multiRequestBuilder.setCompletion {
-                if (it.isSuccess && it.results != null) {
-                    val assets = arrayListOf<Asset>()
-                    it.results.forEach { assets.addAll((it as ListResponse<Asset>).objects) }
-                    hideLoadingDialog()
-                    if (assets.isEmpty()) toast("No assets in this subscription")
-                    else navigate(SubscriptionFragmentDirections.navigateToAssetList(), AssetListFragment.ARG_ASSETS to assets)
-                }
-            }
             clearDebugView()
-            PhoenixApiManager.execute(multiRequestBuilder)
+            viewModel.getAssetsInSubscription(subscriptionChannelsId)
         }
     }
 
@@ -180,6 +131,7 @@ class SubscriptionFragment : DebugFragment(R.layout.fragment_subscription) {
     }
 
     private fun onPackageGetSubscriptionClicked(packageBaseId: Double) {
+        selectedPackageBaseId = packageBaseId
         getSubscriptionRequest(packageBaseId.toInt().toString())
     }
 
---
+++ app/src/main/java/com/kaltura/kflow/presentation/subscription/SubscriptionViewModel.kt
@@ -0,0 +1,92 @@
+package com.kaltura.kflow.presentation.subscription
+
+import androidx.lifecycle.MutableLiveData
+import com.kaltura.client.enums.AssetOrderBy
+import com.kaltura.client.enums.EntityReferenceBy
+import com.kaltura.client.enums.TransactionType
+import com.kaltura.client.services.AssetService
+import com.kaltura.client.services.EntitlementService
+import com.kaltura.client.services.SubscriptionService
+import com.kaltura.client.types.*
+import com.kaltura.client.utils.request.MultiRequestBuilder
+import com.kaltura.kflow.manager.PhoenixApiManager
+import com.kaltura.kflow.presentation.base.BaseViewModel
+import com.kaltura.kflow.utils.Resource
+
+/**
+ * Created by alex_lytvynenko on 2020-01-14.
+ */
+class SubscriptionViewModel : BaseViewModel() {
+
+    val assetList = MutableLiveData<Resource<List<Asset>>>()
+    val assetsInSubscription = MutableLiveData<Resource<List<Asset>>>()
+    val subscriptionList = MutableLiveData<Resource<ArrayList<Subscription>>>()
+
+    fun getPackageList(packageType: String) {
+        val filter = SearchAssetFilter().apply {
+            orderBy = AssetOrderBy.START_DATE_DESC.value
+            kSql = "Base ID > \'0\'"
+            typeIn = packageType
+        }
+
+        val filterPager = FilterPager().apply {
+            pageIndex = 1
+            pageSize = 40
+        }
+
+        PhoenixApiManager.execute(AssetService.list(filter, filterPager).setCompletion {
+            if (it.isSuccess) {
+                if (it.results.objects != null) assetList.value = Resource.Success(it.results.objects)
+            }
+        })
+    }
+
+    fun getEntitlementList() {
+        val filter = EntitlementFilter().apply {
+            entityReferenceEqual = EntityReferenceBy.HOUSEHOLD
+            productTypeEqual = TransactionType.SUBSCRIPTION
+            isExpiredEqual = true
+        }
+
+        val filterPager = FilterPager().apply {
+            pageIndex = 1
+            pageSize = 40
+        }
+
+        PhoenixApiManager.execute(EntitlementService.list(filter, filterPager))
+    }
+
+    fun getSubscription(subscriptionBaseId: String) {
+        val subscriptionFilter = SubscriptionFilter().apply { subscriptionIdIn = subscriptionBaseId }
+        val filterPager = FilterPager().apply {
+            pageIndex = 1
+            pageSize = 40
+        }
+
+        PhoenixApiManager.execute(SubscriptionService.list(subscriptionFilter, filterPager).setCompletion {
+            if (it.isSuccess) subscriptionList.value = Resource.Success(it.results.objects as ArrayList<Subscription>)
+        })
+    }
+
+    fun getAssetsInSubscription(subscriptionChannelsId: ArrayList<Long>) {
+        val multiRequestBuilder = MultiRequestBuilder()
+        val channelFilter = ChannelFilter()
+        val filterPager = FilterPager().apply {
+            pageIndex = 1
+            pageSize = 40
+        }
+
+        subscriptionChannelsId.forEach {
+            channelFilter.idEqual = it.toInt()
+            multiRequestBuilder.add(AssetService.list(channelFilter, filterPager))
+        }
+        multiRequestBuilder.setCompletion {
+            if (it.isSuccess && it.results != null) {
+                val assets = arrayListOf<Asset>()
+                it.results.forEach { assets.addAll((it as ListResponse<Asset>).objects) }
+                assetsInSubscription.value = Resource.Success(assets)
+            }
+        }
+        PhoenixApiManager.execute(multiRequestBuilder)
+    }
+}
