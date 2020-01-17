930aacfecda980f11d944ec1fc3c2e45120863eb
Alexander Litvinenko <a.litvinenko@synergetica.co>
2020-01-17 17:17:02 +0200

    add LiveTvViewModel

---
+++ app/src/main/java/com/kaltura/kflow/presentation/liveTv/LiveTvFragment.kt
@@ -0,0 +1,50 @@
+package com.kaltura.kflow.presentation.liveTv
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.kaltura.client.types.Asset
+import com.kaltura.kflow.R
+import com.kaltura.kflow.presentation.assetList.AssetListFragment
+import com.kaltura.kflow.presentation.debug.DebugFragment
+import com.kaltura.kflow.presentation.debug.DebugView
+import com.kaltura.kflow.presentation.extension.*
+import kotlinx.android.synthetic.main.fragment_live.*
+
+/**
+ * Created by alex_lytvynenko on 17.01.2019.
+ */
+class LiveTvFragment : DebugFragment(R.layout.fragment_live) {
+
+    private val viewModel: LiveTvViewModel by viewModels()
+    private var channels = arrayListOf<Asset>()
+
+    override fun debugView(): DebugView = debugView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showChannel.navigateOnClick(LiveTvFragmentDirections.navigateToAssetList()) { arrayOf(AssetListFragment.ARG_ASSETS to channels) }
+        get.setOnClickListener {
+            hideKeyboard()
+            makeGetChannelsRequest(channelName.string)
+        }
+        channelName.string = "Отр"
+    }
+
+    override fun subscribeUI() {
+        observeResource(viewModel.channelList) {
+            channels = it
+            showChannel.text = getQuantityString(R.plurals.show_channels, channels.size)
+            showChannel.visible()
+        }
+    }
+
+    private fun makeGetChannelsRequest(channelName: String) {
+        withInternetConnection {
+            showChannel.gone()
+            clearDebugView()
+            viewModel.makeGetChannelsRequest(channelName)
+        }
+    }
+}
---
+++ app/src/main/java/com/kaltura/kflow/presentation/liveTv/LiveTvViewModel.kt
@@ -0,0 +1,38 @@
+package com.kaltura.kflow.presentation.liveTv
+
+import androidx.lifecycle.MutableLiveData
+import com.kaltura.client.enums.AssetOrderBy
+import com.kaltura.client.services.AssetService
+import com.kaltura.client.types.Asset
+import com.kaltura.client.types.FilterPager
+import com.kaltura.client.types.SearchAssetFilter
+import com.kaltura.kflow.manager.PhoenixApiManager
+import com.kaltura.kflow.presentation.base.BaseViewModel
+import com.kaltura.kflow.utils.Resource
+
+/**
+ * Created by alex_lytvynenko on 17.01.2020.
+ */
+class LiveTvViewModel : BaseViewModel() {
+
+    val channelList = MutableLiveData<Resource<ArrayList<Asset>>>()
+
+    fun makeGetChannelsRequest(channelName: String) {
+        val filter = SearchAssetFilter().apply {
+            orderBy = AssetOrderBy.START_DATE_DESC.value
+            name = channelName
+            kSql = "(and name~'$channelName' (and (and customer_type_blacklist != '5' (or region_agnostic_user_types = '5' (or region_whitelist = '1077' (and region_blacklist != '1077' (or region_whitelist !+ '' region_whitelist = '0'))))) asset_type='600'))"
+        }
+
+        val filterPager = FilterPager().apply {
+            pageIndex = 1
+            pageSize = 50
+        }
+
+        PhoenixApiManager.execute(AssetService.list(filter, filterPager).setCompletion {
+            if (it.isSuccess) {
+                if (it.results.objects != null) channelList.value = Resource.Success(it.results.objects as ArrayList<Asset>)
+            }
+        })
+    }
+}
