85637533c932b0af67c4fdbaac1b92aae6676ec4
Alex Litvinenko <a.litvinenko@synergetica.co>
2020-03-05 17:36:08 +0200

    redesign Anonymous login

--- app/src/main/java/com/kaltura/kflow/presentation/anonymousLogin/AnonymousLoginFragment.kt
+++ app/src/main/java/com/kaltura/kflow/presentation/anonymousLogin/AnonymousLoginFragment.kt
@@ -2,23 +2,27 @@ package com.kaltura.kflow.presentation.anonymousLogin
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.kaltura.kflow.R
-import com.kaltura.kflow.presentation.debug.DebugFragment
+import com.kaltura.kflow.presentation.base.SharedTransitionFragment
 import com.kaltura.kflow.presentation.debug.DebugView
-import com.kaltura.kflow.presentation.extension.hideKeyboard
-import com.kaltura.kflow.presentation.extension.withInternetConnection
+import com.kaltura.kflow.presentation.extension.*
+import com.kaltura.kflow.presentation.main.Feature
 import com.kaltura.kflow.utils.getUUID
-import kotlinx.android.synthetic.main.fragment_anonymous_login.*
+import kotlinx.android.synthetic.main.fragment_anonymous_login.login
+import kotlinx.android.synthetic.main.view_bottom_debug.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
  * Created by alex_lytvynenko on 11/18/18.
  */
-class AnonymousLoginFragment : DebugFragment(R.layout.fragment_anonymous_login) {
+class AnonymousLoginFragment : SharedTransitionFragment(R.layout.fragment_anonymous_login) {
 
     private val viewModel: AnonymousLoginViewModel by viewModel()
 
     override fun debugView(): DebugView = debugView
+
+    override val feature = Feature.ANONYMOUS_LOGIN
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,12 +32,19 @@ class AnonymousLoginFragment : DebugFragment(R.layout.fragment_anonymous_login) 
         }
     }
 
-    override fun subscribeUI() {}
+    override fun subscribeUI() {
+        observeResource(viewModel.loginRequest,
+                error = { login.error(lifecycleScope) },
+                success = { login.success(lifecycleScope) }
+        )
+    }
 
     private fun makeAnonymousLoginRequest() {
         withInternetConnection {
             clearDebugView()
-            viewModel.anonymousLogin(getUUID(requireContext()))
+            login.startAnimation {
+                viewModel.anonymousLogin(getUUID(requireContext()))
+            }
         }
     }
 }
