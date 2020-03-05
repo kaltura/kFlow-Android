e90d5e55a6228f72a3f0753ef8dc71d6f8214a17
Alex Litvinenko <a.litvinenko@synergetica.co>
2020-03-05 18:04:02 +0200

    redesign Favorites

--- app/src/main/java/com/kaltura/kflow/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/kaltura/kflow/presentation/favorites/FavoritesFragment.kt
@@ -2,21 +2,26 @@ package com.kaltura.kflow.presentation.favorites
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.kaltura.kflow.R
-import com.kaltura.kflow.presentation.debug.DebugFragment
+import com.kaltura.kflow.presentation.base.SharedTransitionFragment
 import com.kaltura.kflow.presentation.debug.DebugView
 import com.kaltura.kflow.presentation.extension.*
+import com.kaltura.kflow.presentation.main.Feature
 import kotlinx.android.synthetic.main.fragment_favorites.*
+import kotlinx.android.synthetic.main.view_bottom_debug.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
  * Created by alex_lytvynenko on 12/13/18.
  */
-class FavoritesFragment : DebugFragment(R.layout.fragment_favorites) {
+class FavoritesFragment : SharedTransitionFragment(R.layout.fragment_favorites) {
 
     private val viewModel: FavoritesViewModel by viewModel()
 
     override fun debugView(): DebugView = debugView
+
+    override val feature = Feature.FAVORITES
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,17 +32,24 @@ class FavoritesFragment : DebugFragment(R.layout.fragment_favorites) {
     }
 
     override fun subscribeUI() {
-        observeResource(viewModel.favoritesCount) {
-            favoriteCount.text = getQuantityString(R.plurals.favorite_count, it)
-            favoriteCount.visible()
-        }
+        observeResource(viewModel.favoritesCount,
+                error = { getFavorites.error(lifecycleScope) },
+                success = {
+                    getFavorites.success(lifecycleScope)
+
+                    favoriteCount.text = getQuantityString(R.plurals.favorite_count, it)
+                    favoriteCount.visible()
+                }
+        )
     }
 
     private fun getFavoritesRequest() {
         withInternetConnection {
             favoriteCount.gone()
             clearDebugView()
-            viewModel.getFavorites()
+            getFavorites.startAnimation {
+                viewModel.getFavorites()
+            }
         }
     }
 }
