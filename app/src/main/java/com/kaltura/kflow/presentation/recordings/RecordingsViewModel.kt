4046e8cf81073264ed3500b3cfdd548fe1482207
Elad Pl <elad.plotski@kaltura.com>
2020-11-02 15:28:54 +0200

    Upgrading Playkit Version 4.10.0

--- app/src/main/java/com/kaltura/kflow/presentation/recordings/RecordingsViewModel.kt
+++ app/src/main/java/com/kaltura/kflow/presentation/recordings/RecordingsViewModel.kt
@@ -2,6 +2,7 @@ package com.kaltura.kflow.presentation.recordings
 
 import androidx.lifecycle.MutableLiveData
 import com.kaltura.client.services.RecordingService
+import com.kaltura.client.types.FilterPager
 import com.kaltura.client.types.Recording
 import com.kaltura.kflow.manager.PhoenixApiManager
 import com.kaltura.kflow.presentation.base.BaseViewModel
@@ -15,7 +16,11 @@ class RecordingsViewModel(private val apiManager: PhoenixApiManager) : BaseViewM
     val recordingList = MutableLiveData<Resource<ArrayList<Recording>>>()
 
     fun getRecordings() {
-        apiManager.execute(RecordingService.list().setCompletion {
+        val filterPager = FilterPager().apply {
+            pageIndex = 1
+            pageSize = 100
+        }
+        apiManager.execute(RecordingService.list(null,filterPager).setCompletion() {
             if (it.isSuccess) {
                 if (it.results.objects != null) recordingList.value = Resource.Success(it.results.objects as ArrayList<Recording>)
                 else recordingList.value = Resource.Success(arrayListOf())
