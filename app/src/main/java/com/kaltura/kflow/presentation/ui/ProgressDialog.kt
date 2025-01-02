c070d795b7f67f8e2f62628d976f227535169b83
Elad Pi <>
2025-01-02 16:07:33 +0200

    Remove jcenter Replace CircularProgressButton

--- app/src/main/java/com/kaltura/kflow/presentation/ui/ProgressDialog.kt
+++ app/src/main/java/com/kaltura/kflow/presentation/ui/ProgressDialog.kt
@@ -9,7 +9,7 @@ import com.kaltura.kflow.R
 /**
  * Created by alex_lytvynenko on 2019-07-22.
  */
-class ProgressDialog(context: Context?) : AppCompatDialog(context) {
+class ProgressDialog(context: Context?) : AppCompatDialog(context!!) {
 
     init {
         setContentView(R.layout.dialog_progress)
