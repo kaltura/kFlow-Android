2b22ac1e4efa062a95c96cfb35c723feccc627fa
Elad Pi <>
2026-01-27 10:45:52 +0200

    Fix Transaction History

--- app/src/main/java/com/kaltura/kflow/presentation/transactionHistory/TransactionHistoryListAdapter.kt
+++ app/src/main/java/com/kaltura/kflow/presentation/transactionHistory/TransactionHistoryListAdapter.kt
@@ -1,11 +1,15 @@
 package com.kaltura.kflow.presentation.transactionHistory
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.kaltura.client.types.BillingTransaction
 import com.kaltura.kflow.R
+import com.kaltura.kflow.databinding.ItemProductPriceBinding
+import com.kaltura.kflow.databinding.ItemRecordingBinding
 import com.kaltura.kflow.presentation.extension.inflate
+import com.kaltura.kflow.presentation.recordingList.RecordingListAdapter
 import kotlinx.android.extensions.LayoutContainer
 
 /**
@@ -19,18 +23,21 @@ class TransactionHistoryListAdapter : RecyclerView.Adapter<TransactionHistoryLis
             transactions.addAll(value)
             notifyDataSetChanged()
         }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyViewHolder(parent.inflate(R.layout.item_product_price))
+    private var _binding: ItemProductPriceBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val bindMe = ItemProductPriceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(bindMe,parent)
+    }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) = holder.bind(transactions[position])
 
     override fun getItemCount() = transactions.size
-
-    inner class MyViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    inner class MyViewHolder(val binding: ItemProductPriceBinding,override val containerView: View) : RecyclerView.ViewHolder(binding.root), LayoutContainer {
 
         fun bind(billingTransaction: BillingTransaction) {
-            //mType.setText("Product Type: " + productPrice.getProductType().getValue());
-            //mPrice.setText("Price: " + productPrice.getPrice().getCurrencySign() + productPrice.getPrice().getAmount());
+            binding.productPriceType.setText("Product Type: " + billingTransaction.billingPriceType);
+            binding.price.setText("Price: " + billingTransaction.getPrice().getCurrencySign() + billingTransaction.getPrice().getAmount());
         }
     }
 }
